2e3ce8929061f3fda340266ec78bbb5e39b6dfeb
Riot <edu.buntu.unix@gmail.com>
2023-04-18 20:58:19 +0200

    Rotation settings added.

--- RiotsRotations/Melee/DRG_Highwind.cs
+++ RiotsRotations/Melee/DRG_Highwind.cs
@@ -11,7 +11,10 @@
 
         protected override IRotationConfigSet CreateConfiguration()
         {
-            return base.CreateConfiguration();
+            return base.CreateConfiguration()
+                .SetBool("DRG_OpenerAt90", false, "Use Lvl. 90 opener (ignores other settings during the opener itself)")
+                .SetBool("DRG_KeepBuffsAligned", false, "Try to keep buffs aligned with Geirskogul in case of drifting. Statically 6 second remaining on GSK for alignment.")
+                .SetBool("DRG_LanceChargeFirst", false, "Move Lance Charge to in front of True Thrust for opener. Not entirely compatible with the above setting.");
         }
 
         protected override bool AttackAbility(byte abilitiesRemaining, out IAction act)
