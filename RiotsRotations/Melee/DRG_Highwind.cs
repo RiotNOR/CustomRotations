3e6abf9da257d6904d034a449b46256de6184a08
Riot <edu.buntu.unix@gmail.com>
2023-04-18 20:55:57 +0200

    General Rotation file created.

--- RiotsRotations/Melee/DRG_Highwind.cs
+++ RiotsRotations/Melee/DRG_Highwind.cs
@@ -1,7 +1,29 @@
 ﻿namespace Melee
 {
+    [SourceCode("https://github.com/RiotNOR/CustomRotations/blob/main/RiotsRotations/Melee/DRG_Highwind.cs")]
     internal class DRG_Highwind : DRG_Base
     {
+        public override string GameVersion => "6.38";
 
+        public override string RotationName => "Riot's Highwind";
+
+        public override string Description => "A rotation with an opener following The Balance, and more.";
+
+        protected override IRotationConfigSet CreateConfiguration()
+        {
+            return base.CreateConfiguration();
+        }
+
+        protected override bool AttackAbility(byte abilitiesRemaining, out IAction act)
+        {
+            act = null;
+            return false;
+        }
+
+        protected override bool GeneralGCD(out IAction act)
+        {
+            act = null;
+            return false;
+        }
     }
 }
