0c2cba7dcb8997a4ced5b4f371ab27b27408fb01
Riot <edu.buntu.unix@gmail.com>
2023-04-21 10:35:41 +0200

    Added some comments, new setting for 5th hit

--- RiotsRotations/Melee/DRG_Highwind.cs
+++ RiotsRotations/Melee/DRG_Highwind.cs
@@ -22,6 +22,9 @@ namespace Melee
 
         private static DRGGauge JobGauge => Service.JobGauges.Get<DRGGauge>();
 
+        // Just makes sure our opener can actually be ran and complete.
+        // @TODO: Add check that we have no eyes, or handle that
+        // inside the actual opener (probably best to do it that way).
         private void HandleOpenerAvailability(out IAction act)
         {
             if (Configs.GetBool("DRG_OpenerAt90")
@@ -42,30 +45,39 @@ namespace Melee
             act = null;
         }
 
+        // No constructor, so we use this CreateConfiguration() method
+        // to setup our settings.
         protected override IRotationConfigSet CreateConfiguration()
         {
             return base.CreateConfiguration()
                 .SetBool("DRG_OpenerAt90", false, "Use Lvl. 90 opener (ignores other settings during the opener itself)")
                 .SetBool("DRG_KeepBuffsAligned", false, "Try to keep buffs aligned with Geirskogul in case of drifting. Statically 6 second remaining on GSK for alignment.")
-                .SetBool("DRG_LanceChargeFirst", false, "Move Lance Charge to in front of True Thrust for opener. Not entirely compatible with the above setting.");
+                .SetBool("DRG_LanceChargeFirst", false, "Move Lance Charge to in front of True Thrust for opener. Not entirely compatible with the above setting.")
+                .SetBool("DRG_LifeSurgeFifthHit", false, "Buff fifth combo hit with Life Surge IF you're in position or have True North on.");
         }
 
         #region GCD actions
+
+        // General cooldowns (Global cooldowns)
         protected override bool GeneralGCD(out IAction act)
         {
-            // Always start with Chaotic Spring combo upon
-            // entering combat.
+            // If we're not in combat, we reset our combo path.
+            // @TODO: Make sure we have no active combo going.
             if (!InCombat)
             {
                 GoThroughFirstPath = true;
             }
 
+            // These will only run if AoE is enabled.
+            // Also, Dragoon AOEs only run if there are 3 or more mobs nearby.
             if (CoerthanTorment.CanUse(out act)) return true;
             if (SonicThrust.CanUse(out act)) return true;
             if (DoomSpike.CanUse(out act)) return true;
 
             // Make sure we alternate between Chaotic Spring combo
-            // and Heavens' Thrust combo
+            // and Heavens' Thrust combo.
+            // @TODO: Add check for NumberOfHostilesInRange == 2 to apply
+            // Chaotic Spring debuff on the enemies for increased DPS.
             if (GoThroughFirstPath)
             {
                 if (IsLastGCD(ActionID.Disembowel)) GoThroughFirstPath = false;
@@ -81,14 +93,16 @@ namespace Melee
             if (WheelingThrust.CanUse(out act)) return true;
             if (FangandClaw.CanUse(out act)) return true;
 
-            // Heavens' Thrust
+            // Heavens' Thrust combo
             if (HeavensThrust.CanUse(out act)) return true;
             if (FullThrust.CanUse(out act)) return true;
 
             // Chaotic Spring
             if (ChaosThrust.CanUse(out act)) return true;
+            //if (UseBurstMedicine(out act)) return true;
             if (TrueThrust.CanUse(out act)) return true;
 
+            // Ranged uptime. Toggleable in Actions by the player.
             if (PiercingTalon.CanUse(out act)) return true;
 
             act = null;
@@ -98,14 +112,17 @@ namespace Melee
         //For some gcds very important, even more than healing, defense, interrupt, etc.
         protected override bool EmergencyGCD(out IAction act)
         {
+            // EmergencyGCD is ran first, ref RotationSolver.Basic.CustomRotation, IAction GCD method
+            // so we just run this here.
             HandleOpenerAvailability(out act);
 
-            // Ehh?
+            // Ehh? Probably shouldn't have this as an option.
             if (IsOpenerAvailable
                 && Configs.GetBool("DRG_LanceChargeFirst")
                 && LanceCharge.CanUse(out act, CanUseOption.MustUse)) return true;
 
-            //We are now going to run opener rotation.
+            // We are now telling every other function that the Opener
+            // can and is being run.
             if (Level == 90
                 && Configs.GetBool("DRG_OpenerAt90")
                 && IsOpenerAvailable)
@@ -118,6 +135,7 @@ namespace Melee
         #endregion
 
         #region 0GCD actions
+        // Attack ABILITY. Not attack SKILLS. ;)
         protected override bool AttackAbility(out IAction act)
         {
             if (Level == 90
@@ -127,12 +145,8 @@ namespace Melee
                 return AttackAbilityOpener(out act);
             }
 
-            //if (JobGauge.EyeCount == 2
-            //    && Player.HasStatus(true, StatusID.LanceCharge)
-            //    && Geirskogul.CanUse(out act, CanUseOption.MustUse)
-            //    || JobGauge.EyeCount < 2
-            //    && Geirskogul.CanUse(out act, CanUseOption.MustUse)) return true;
-
+            // Just to make sure Stardiver doesn't get doubleweaved.
+            // @TODO: Do this in a better way.
             if (!IsLastAction(false, StarDiver))
             {
                 if (Nastrond.CanUse(out act, CanUseOption.MustUse)) return true;
@@ -141,6 +155,8 @@ namespace Melee
                 if (SpineShatterDive.CanUse(out act, CanUseOption.MustUseEmpty)) return true;
 
                 if (WyrmwindThrust.CanUse(out act, CanUseOption.MustUse)) return true;
+
+                // Trying not to double weave High Jump and Mirage Dive.
                 if (!IsLastAction(false, HighJump) && MirageDive.CanUse(out act)) return true;
 
                 if (HighJump.EnoughLevel)
@@ -153,6 +169,7 @@ namespace Melee
                 }
             }
 
+            // Don't want to double weave with a buff either.
             if (!IsLastAction(false, LifeSurge)
                 && StarDiver.CanUse(out act, CanUseOption.MustUse)) return true;
 
@@ -162,6 +179,10 @@ namespace Melee
 
         private bool AttackAbilityOpener(out IAction act)
         {
+            // Wyrmwind Thrust ends our opener, and thus lets another
+            // function know that we can disable the opener and run things
+            // off cooldown.
+            // @TODO: Should make more checks in case of reopeners and/or filler rotations.
             if (ShouldEndOpener) IsCurrentlyInOpener = false;
 
             // Use Wyrmwind Thrust after Raiden Thrust #2 in 0GCD position 1
@@ -236,49 +257,74 @@ namespace Melee
                 return EmergencyAbilityOpener(nextGCD, out act);
             }
 
-
             if (nextGCD is BaseAction action)
             {
+                // We don't want Geirskogul to run without Lance Charge if we're
+                // close to going into Life of the Dragon.
+                if (JobGauge.EyeCount == 2
+                        && Player.HasStatus(true, StatusID.LanceCharge)
+                        && Geirskogul.CanUse(out act, CanUseOption.MustUse)) return true;
+
+                // If player has disabled Automatic Burst, these will not be ran.
+                // This is how you will control burst in harder content to align with your teammates.
+                // Remember, Geirskogul will not run if you have 2 eyes open.
                 if (InBurst)
                 {
+                    // Geirskogul can drift a bit. Unsure why, but to combat that until I understand
+                    // more of creating these rotations we'll align it within 6 seconds of Geirskogul IF the user
+                    // has ticked the checkbox for this.
+                    // Ref: CreateConfiguration()
                     if (Configs.GetBool("DRG_KeepBuffsAligned")
-                        && Geirskogul.ElapsedAfter(24))
+                        && (Geirskogul.ElapsedAfter(24) || !Geirskogul.IsCoolingDown))
                     {
-                        if (LanceCharge.CanUse(out act, CanUseOption.MustUse)) return true;
                         if (DragonSight.CanUse(out act, CanUseOption.MustUse)) return true;
                         if (BattleLitany.CanUse(out act, CanUseOption.MustUse)) return true;
+                        if (LanceCharge.CanUse(out act, CanUseOption.MustUse)) return true;
                     }
                     else if (!Configs.GetBool("DRG_KeepBuffsAligned"))
                     {
-                        if (LanceCharge.CanUse(out act, CanUseOption.MustUse)) return true;
                         if (DragonSight.CanUse(out act, CanUseOption.MustUse)) return true;
                         if (BattleLitany.CanUse(out act, CanUseOption.MustUse)) return true;
+                        if (LanceCharge.CanUse(out act, CanUseOption.MustUse)) return true;
                     }
                 }
 
-                if (JobGauge.EyeCount == 2
-                    && Player.HasStatus(true, StatusID.LanceCharge)
-                    && Geirskogul.CanUse(out act, CanUseOption.MustUse)
-                    || JobGauge.EyeCount < 2
+                // If we have less than 2 eyes open (0 or 1), feel free to spam Geirskogul.
+                if (JobGauge.EyeCount < 2
                     && Geirskogul.CanUse(out act, CanUseOption.MustUse)) return true;
 
                 // Make sure Heavens' Thrust gets buffed
                 if (nextGCD.IsTheSameTo(false, HeavensThrust, FullThrust)
                     && LifeSurge.CanUse(out act, CanUseOption.EmptyOrSkipCombo)) return true;
 
-                // We buff 5th hit IF we're inside positional
-                // @TODO verify this as I'm too lazy at the time of writing
-                if (action.EnemyPositional != EnemyPositional.None
-                    && action.Target != null)
+                if (Configs.GetBool("DRG_LifeSurgeFifthHit"))
                 {
-                    if (action.EnemyPositional == action.Target.FindEnemyPositional() && action.Target.HasPositional())
+                    // We buff 5th hit only IF we're inside positional.
+                    if (action.EnemyPositional != EnemyPositional.None
+                        && action.Target != null)
                     {
-                        if (IsLastGCD(false, WheelingThrust)
-                            && nextGCD.IsTheSameTo(false, FangandClaw)
-                                || IsLastGCD(false, FangandClaw)
-                                && nextGCD.IsTheSameTo(false, WheelingThrust))
+                        // If in position for a positional
+                        if (action.EnemyPositional == action.Target.FindEnemyPositional() && action.Target.HasPositional())
                         {
-                            return LifeSurge.CanUse(out act, CanUseOption.EmptyOrSkipCombo);
+                            if (IsLastGCD(false, WheelingThrust)
+                                && nextGCD.IsTheSameTo(false, FangandClaw)
+                                    || IsLastGCD(false, FangandClaw)
+                                    && nextGCD.IsTheSameTo(false, WheelingThrust))
+                            {
+                                return LifeSurge.CanUse(out act, CanUseOption.EmptyOrSkipCombo);
+                            }
+                        }
+
+                        // Or if we have True North
+                        if (Player.HasStatus(true, StatusID.TrueNorth))
+                        {
+                            if (IsLastGCD(false, WheelingThrust)
+                                && nextGCD.IsTheSameTo(false, FangandClaw)
+                                    || IsLastGCD(false, FangandClaw)
+                                    && nextGCD.IsTheSameTo(false, WheelingThrust))
+                            {
+                                return LifeSurge.CanUse(out act, CanUseOption.EmptyOrSkipCombo);
+                            }
                         }
                     }
                 }
